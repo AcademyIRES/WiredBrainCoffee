25004cfd986cf88dd1fdf5b06eb4c348cd9a63e1
Mekni Oussama <oussama.mekni.external@ynap.com>
2025-03-05 00:33:00 +0100

    Implementing Different Unit Tests

--- WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
+++ WiredBrainCoffee.DataProcessorTests/Parsing/CsvLineParserTests.cs
@@ -1,18 +1,49 @@
-﻿namespace WiredBrainCoffee.DataProcessor.Parsing
+﻿namespace WiredBrainCoffee.DataProcessor.Parsing;
+
+public class CsvLineParserTests
 {
-    public class CsvLineParserTests
+    [Fact]
+    public void ShouldParseValidLine()
     {
-        [Fact]
-        public void ShouldParseValidLine()
-        {
-            string[] csvLines = new[] { "Cappuccino;10/27/2022 8:06:04 AM" };
+        // Arrange
+        string[] csvLines = new[] { "Cappuccino;10/27/2022 8:06:04 AM" };
 
-            var machineDataItems = CsvLineParser.Parse(csvLines);
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
 
-            Assert.NotNull(machineDataItems);
-            Assert.Single(machineDataItems);
-            Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
-            Assert.Equal(new DateTime(2022, 10, 27, 8, 6, 4), machineDataItems[0].CreatedAt);
-        }
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Single(machineDataItems);
+        Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+        Assert.Equal(new DateTime(2022, 10, 27, 8, 6, 4), machineDataItems[0].CreatedAt);
+    }
+
+    [Fact]
+    public void ShouldSkipEmptyLines()
+    {
+        // Arrange
+        string[] csvLines = new[] { "", "  " };
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Empty(machineDataItems);
+    }
+
+    [InlineData("Cappuccino", "Invalid csv line")]
+    [InlineData("Cappuccino;InvalidDateTime", "Invalid datetime in csv line")]
+    [Theory]
+    public void ShouldThrowExceptionForInvalidLine(string csvLine,
+        string expectedMessagePrefix)
+    {
+        // Arrange
+        var csvLines = new[] { csvLine };
+
+        // Act and Assert 
+        var exception = Assert.Throws<Exception>(() => CsvLineParser.Parse(csvLines));
+
+        Assert.Equal($"{expectedMessagePrefix}: {csvLine}", exception.Message);
     }
 }
